7e0e2f62d949dec53846829af2926563c2cf96b0
Luis Corales <luisaugcorales@gmail.com>
2021-05-13 19:04:59 -0500

    Update patients.js

--- src/routes/patients.js
+++ src/routes/patients.js
@@ -1,122 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const mysql = require("mysql");
-
-// Create connection to MySQL
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-});
-
-db.connect((err) => {
-    if(err) {
-        throw err;
-    }
-
-    console.log("MySQL connected!")
-});
+const patientsController = require("../controllers/patientsController");
 
 // GET all patients
-router.get("/", (req, res) => {
-    let sql = 'SELECT * FROM medicaldb.patient';
-
-    db.query(sql, (err, result) => {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.status(200).json({
-                message: 'GET request to /patients',
-                patients: result
-            });
-            console.log("All patients printed!");
-        }
-    });
-});
+router.get("/", patientsController.getAll);
 
 // POST a new patient
-router.post("/", (req, res) => {
-    let patientData = {
-        firstName: req.body.firstName,
-        surname: req.body.surname,
-        pathology: req.body.pathology
-    }
-    let sql = 'INSERT INTO medicaldb.patient SET ?';
-
-    db.query(sql, patientData, (err) => {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.status(200).json({
-                message: 'POST request to /patients',
-                createdPatient: patientData
-            });
-            console.log("A new patient was inserted!");
-        }
-    });  
-}); 
+router.post("/", patientsController.post);
 
 // GET a patient by id
-router.get("/:patientId", (req, res) => {
-    let id = req.params.patientId;
-
-    let sql = 'SELECT * FROM medicaldb.patient WHERE id = ' + id;
-
-    db.query(sql, (err, result) => {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.status(200).json({
-                message: 'GET request to /patients/' + id,
-                patient: result
-            });
-            console.log("Printed patient with id = " + id);
-        }
-    });
-});
+router.get("/:patientId", patientsController.getOne);
 
 // PUT/UPDATE a patient by id
-router.put("/:patientId", (req, res) => {
-    let id = req.params.patientId;
-    let patientData = {
-        firstName: req.body.firstName,
-        surname: req.body.surname,
-        pathology: req.body.pathology
-    }
-
-    let sql = 'UPDATE medicaldb.patient SET ? WHERE id = ' + id;
-
-    db.query(sql, patientData, (err) => {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.status(200).json({
-                message: 'PUT request to /patients/' + id,
-                updatedPatient: patientData
-            });
-            console.log("Updated patient with id = " + id);
-        }
-    });  
-});
+router.put("/:patientId", patientsController.put);
 
 // DELETE a patient
-router.delete("/:patientId", (req, res) => {
-    let id = req.params.patientId;
-
-    let sql = 'DELETE FROM medicaldb.patient WHERE id = ' + id;
-
-    db.query(sql, (err, result) => {
-        if(err) {
-            console.log(err.message);
-        } else {
-            res.status(200).json({
-                message: 'DELETE request to /patients/' + id,
-                deletedPatient: result
-            });
-            console.log("Deleted patient with id = " + id);
-        }
-    }); 
-});
+router.delete("/:patientId", patientsController.delete);
 
 module.exports = router;
