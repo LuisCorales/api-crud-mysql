26324f0d417943f2aaa5e87ac0f2f84e83f311f9
Luis Corales <luisaugcorales@gmail.com>
2021-05-13 19:04:08 -0500

    Create hospitalsController.js

---
+++ src/controllers/hospitalsController.js
@@ -0,0 +1,101 @@
+const database = require("../db/database");
+const db = database.db;
+
+// All the functions will be executed when a route is called
+
+// To GET route
+exports.getAll = (req, res) => {
+    let sql = 'SELECT * FROM medicaldb.hospital';
+
+    db.query(sql, (err, result) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.status(200).json({
+                message: 'GET request to /hospitals',
+                hospitals: result
+            });
+            console.log("All hospitals printed!");
+        }
+    });
+};
+
+// To POST route
+exports.post = (req, res) => {
+    let hospitalData = {
+        name: req.body.name
+    }
+    let sql = 'INSERT INTO medicaldb.hospital SET ?';
+
+    db.query(sql, hospitalData, (err) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.status(200).json({
+                message: 'POST request to /hospitals',
+                createdHospital: hospitalData
+            });
+            console.log("A new hospital was inserted!");
+        }
+    });  
+}; 
+
+// To GET by id route
+exports.getOne = (req, res) => {
+    let id = req.params.hospitalId;
+
+    let sql = 'SELECT * FROM medicaldb.hospital WHERE id = ' + id;
+
+    db.query(sql, (err, result) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.status(200).json({
+                message: 'GET request to /hospital/' + id,
+                hospital: result
+            });
+            console.log("Printed hospital with id = " + id);
+        }
+    });
+};
+
+// To PUT route
+exports.put = (req, res) => {
+    let id = req.params.hospitalId;
+    let hospitalData = {
+        name: req.body.name
+    }
+
+    let sql = 'UPDATE medicaldb.hospital SET ? WHERE id = ' + id;
+
+    db.query(sql, hospitalData, (err) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.status(200).json({
+                message: 'PUT request to /hospitals/' + id,
+                updatedHospital: hospitalData
+            });
+            console.log("Updated hospital with id = " + id);
+        }
+    }); 
+};
+
+// To DELETE route
+exports.delete = async (req, res) => {
+    let id = req.params.hospitalId;
+
+    let sql = 'DELETE FROM medicaldb.hospital WHERE id = ' + id;
+
+    db.query(sql, (err, result) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            res.status(200).json({
+                message: 'DELETE request to /hospitals/' + id,
+                deletedHospital: result
+            });
+            console.log("Deleted hospital with id = " + id);
+        }
+    }); 
+};
