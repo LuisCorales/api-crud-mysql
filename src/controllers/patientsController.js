9796c3e19382555f58ed602792f449aff9fb2e60
Luis Corales <luisaugcorales@gmail.com>
2021-05-16 16:59:21 -0500

    Update patientsController.js

--- src/controllers/patientsController.js
+++ src/controllers/patientsController.js
@@ -1,125 +1,152 @@
 const database = require("../db/database");
 const db = database.db;
 
+// If there is an error, send to response
+const sendError = (res, e) => {
+    return res.status(404).json({
+        message: 'There was a problem...',
+        error: e.message
+    });
+};
+
 // All the functions will be executed when a patients route is called
 
 // To GET patients route
 exports.getAll = (req, res) => {
-    let sql = 'SELECT * FROM medicaldb.patient';
+    try {
+        let sql = 'SELECT * FROM medicaldb.patient';
 
-    db.query(sql, (err, result) => {
-        if(err) {
-            res.status(404).json({
-                message: 'There was a problem...',
-                error: err.message
-            });
-            console.log(err.message);
-        } else {
-            res.status(200).json({
+        db.query(sql, (err, result) => {
+            if(err) {
+                throw err;
+            }
+            
+            return res.status(200).json({
                 message: 'GET request to /patients',
                 patients: result
             });
-            console.log("All patients printed!");
-        }
-    });
+        });
+    } catch(e) {
+        sendError(res, e);
+    }
 };
 
 // To POST patients route
 exports.post = (req, res) => {
-    let patientData = {
-        firstName: req.body.firstName,
-        surname: req.body.surname,
-        pathology: req.body.pathology
-    }
-    let sql = 'INSERT INTO medicaldb.patient SET ?';
+    try {
+        if(req.body.firstName == null || req.body.surname == null)
+        {
+            throw new Error("firstName or surname cannot be NULL");
+        }
 
-    db.query(sql, patientData, (err) => {
-        if(err) {
-            res.status(404).json({
-                message: 'There was a problem...',
-                error: err.message
-            });
-            console.log(err.message);
-        } else {
-            res.status(200).json({
+        let patientData = {
+            firstName: req.body.firstName,
+            surname: req.body.surname,
+            pathology: req.body.pathology
+        }
+        let sql = 'INSERT INTO medicaldb.patient SET ?';
+
+        db.query(sql, patientData, (err) => {
+            if(err) {
+                throw err;
+            } 
+            
+            return res.status(200).json({
                 message: 'POST request to /patients',
                 createdPatient: patientData
             });
-            console.log("A new patient was inserted!");
-        }
-    });  
+        });  
+    } catch(e) {
+        sendError(res, e);
+    }
 }; 
 
 // To GET by id patients route
 exports.getOne = (req, res) => {
-    let id = req.params.patientId;
+    try {
+        if(isNaN(req.params.patientId))
+        {
+            throw new Error("The id must be a number")
+        }
+        
+        let id = req.params.patientId;
 
-    let sql = 'SELECT * FROM medicaldb.patient WHERE id = ' + id;
+        let sql = 'SELECT * FROM medicaldb.patient WHERE id = ' + id;
 
-    db.query(sql, (err, result) => {
-        if(err) {
-            res.status(404).json({
-                message: 'There was a problem...',
-                error: err.message
-            });
-            console.log(err.message);
-        } else {
-            res.status(200).json({
+        db.query(sql, (err, result) => {
+            if(err) {
+                throw err;
+            } 
+            
+            return res.status(200).json({
                 message: 'GET request to /patients/' + id,
                 patient: result
             });
-            console.log("Printed patient with id = " + id);
-        }
-    });
+        });
+    } catch(e) {
+        sendError(res, e);
+    }
 };
 
 // To PUT patients route
 exports.put = (req, res) => {
-    let id = req.params.patientId;
-    let patientData = {
-        firstName: req.body.firstName,
-        surname: req.body.surname,
-        pathology: req.body.pathology
-    }
+    try {
+        if(isNaN(req.params.patientId))
+        {
+            throw new Error("The id must be a number")
+        }
 
-    let sql = 'UPDATE medicaldb.patient SET ? WHERE id = ' + id;
+        if(req.body.firstName == null || req.body.surname == null)
+        {
+            throw new Error("firstName or surname cannot be NULL");
+        }
 
-    db.query(sql, patientData, (err) => {
-        if(err) {
-            res.status(404).json({
-                message: 'There was a problem...',
-                error: err.message
-            });
-            console.log(err.message);
-        } else {
-            res.status(200).json({
+        let id = req.params.patientId;
+        let patientData = {
+            firstName: req.body.firstName,
+            surname: req.body.surname,
+            pathology: req.body.pathology
+        }
+
+        let sql = 'UPDATE medicaldb.patient SET ? WHERE id = ' + id;
+        db.query(sql, patientData, (err) => {
+            if(err) {
+                throw err;
+            } 
+            
+            return res.status(200).json({
                 message: 'PUT request to /patients/' + id,
                 updatedPatient: patientData
             });
-            console.log("Updated patient with id = " + id);
-        }
-    });  
+        });  
+    } catch(e) {
+        sendError(res, e);
+    }
 };
 
 // To DELETE patients route
-exports.delete = async (req, res) => {
-    let id = req.params.patientId;
+exports.delete = (req, res) => {
+    try {
+        if(isNaN(req.params.patientId))
+        {
+            throw new Error("The id must be a number")
+        }
 
-    let sql = 'DELETE FROM medicaldb.patient WHERE id = ' + id;
+        let id = req.params.patientId;
 
-    db.query(sql, (err, result) => {
-        if(err) {
-            res.status(404).json({
-                message: 'There was a problem...',
-                error: err.message
-            });
-            console.log(err.message);
-        } else {
-            res.status(200).json({
+        let sql = 'DELETE FROM medicaldb.patient WHERE id = ' + id;
+
+        db.query(sql, (err, result) => {
+            if(err) {
+                throw err;
+            }
+            
+            return res.status(200).json({
                 message: 'DELETE request to /patients/' + id,
                 deletedPatient: result
             });
-            console.log("Deleted patient with id = " + id);
-        }
-    }); 
+        }); 
+    } catch(e) {
+        sendError(res, e);
+    }
 };
